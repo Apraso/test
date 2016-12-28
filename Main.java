98399dbfd7d39f1858a7726064312d72178fab2c
Apraso <x3e1ad5cc@163.com>
2016-12-28 09:13:06 +0800

    Update Main.java
    Add annotations

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
-
+//This is a test.
+/**
+* The following three methods are to calculate the nth number in a Fibonacci sequence.
+* Assume 0<=n<=45.
+*/
 public class Main {
 	public static int[] arr=new int [50];
+	//The recursive method, with a complexity of O(2^n)
 	public static int recursiveMethod(int n){
 		if(n<0) return 0;
 		if(n==0)return 1;
 		if(n==1)return 1;
 		return recursiveMethod(n-1)+recursiveMethod(n-2);
 	}
+	//The method using an array, with a complexity of O(n)
 	public static int arrayMethod(int n){
 		if(n<0) return 0;
 		if(n==0) return 1;
@@ -17,6 +23,7 @@ public class Main {
 		}
 		return arr[n];
 	}
+	//The look-up method using recursion and an array, with a complexity of O(n)
 	public static int lookupMethod(int n){
 		if(n<0) return 0;
 		if(n==0)arr[0]=1;
@@ -24,8 +31,10 @@ public class Main {
 		if(arr[n]==0)return (arr[n]=lookupMethod(n-1)+lookupMethod(n-2));
 		return arr[n];
 	}
+	//An instance
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Test the time to run each method
 		long timeElapsed=System.currentTimeMillis();
 		System.out.println(recursiveMethod(40));
 		timeElapsed=System.currentTimeMillis()-timeElapsed;
